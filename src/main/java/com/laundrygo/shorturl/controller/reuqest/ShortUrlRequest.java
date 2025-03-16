53fee2c7a6f400012e009999d55321d1c15f6138
junyong.seon <ssjy4974@naver.com>
2025-03-17 00:33:37 +0900

    [test] short-url api test

--- src/main/java/com/laundrygo/shorturl/controller/reuqest/ShortUrlRequest.java
+++ src/main/java/com/laundrygo/shorturl/controller/reuqest/ShortUrlRequest.java
@@ -1,13 +1,17 @@
 package com.laundrygo.shorturl.controller.reuqest;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ShortUrlRequest {
+	@NotBlank(message = "url은 필수 값입니다.")
 	@Pattern(regexp = "^(https?)://([a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*)(?::\\d+)?(/.*)?$", message = "유효하지 않은 URL 입니다.")
 	private String originUrl;
 }
---
+++ src/test/java/com/laundrygo/shorturl/controller/ShortUrlControllerTest.java
@@ -0,0 +1,62 @@
+package com.laundrygo.shorturl.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+import com.laundrygo.shorturl.ControllerTestSupport;
+import com.laundrygo.shorturl.controller.reuqest.ShortUrlRequest;
+
+class ShortUrlControllerTest extends ControllerTestSupport {
+
+	@Test
+	@DisplayName("원본 url을 입력받아 단축 url을 반환한다. (정상응답)")
+	void getShortUrl() throws Exception {
+		// given
+		String originUrl = "http://www.naver.com";
+		ShortUrlRequest request = new ShortUrlRequest(originUrl);
+		// when then
+		mockMvc.perform(post("/api/v1/short-url").contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(request))).andExpect(status().isOk());
+	}
+
+	@Test
+	@DisplayName("원본 URL이 유효하지않은경우 요청은 실패한다. (null)")
+	void getShortUrlWithInvalidParam_null() throws Exception {
+		// given
+		String originUrl = null;
+		ShortUrlRequest request = new ShortUrlRequest(originUrl);
+		// when then
+		mockMvc.perform(post("/api/v1/short-url").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))).andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.message").value("url은 필수 값입니다."));
+	}
+
+	@Test
+	@DisplayName("원본 URL이 유효하지않은경우 요청은 실패한다. (empty)")
+	void getShortUrlWithInvalidParam_empty() throws Exception {
+		// given
+		String originUrl = "";
+		ShortUrlRequest request = new ShortUrlRequest(originUrl);
+		// when then
+		mockMvc.perform(post("/api/v1/short-url").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))).andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.message").value("url은 필수 값입니다."));
+	}
+
+	@Test
+	@DisplayName("원본 URL이 유효하지않은경우 요청은 실패한다. (InvalidUrl)")
+	void getShortUrlWithInvalidParam_InvalidUrl() throws Exception {
+		// given
+		String originUrl = "abcdswq";
+		ShortUrlRequest request = new ShortUrlRequest(originUrl);
+		// when then
+		mockMvc.perform(post("/api/v1/short-url").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request))).andExpect(status().isBadRequest())
+			.andExpect(jsonPath("$.message").value("유효하지 않은 URL 입니다."));
+	}
+
+}
