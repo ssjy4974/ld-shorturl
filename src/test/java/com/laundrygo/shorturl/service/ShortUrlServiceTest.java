51aa6f20c4d5a9dec02957be96979a7c8b850fed
junyong.seon <ssjy4974@naver.com>
2025-03-19 02:04:05 +0900

    [test] getOriginUrl test
    - 단축url로 원본 url 조회 성공, 실패 테스트

--- src/test/java/com/laundrygo/shorturl/service/ShortUrlServiceTest.java
+++ src/test/java/com/laundrygo/shorturl/service/ShortUrlServiceTest.java
@@ -2,6 +2,8 @@ package com.laundrygo.shorturl.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.NoSuchElementException;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,5 +34,38 @@ class ShortUrlServiceTest {
 		assertNotNull(shortUrl);
 		assertEquals(8, shortUrl.length());
 		assertEquals(shortUrl, urlMapping.getShortUrl());
+	}
+
+	@Test
+	@DisplayName("단축URL로 조회시 원본 URL을 반환한다.")
+	public void getOriginUrl_success() {
+		// given:
+		String originUrl = "https://www.naver.com";
+		String shortUrl = "abcdefgh";
+		UrlMapping urlMapping = UrlMapping.create(originUrl, shortUrl);
+
+		UrlMapping savedUrl = urlMappingRepository.save(urlMapping);
+
+		// when:
+		UrlMapping result = shortUrlService.getOriginUrl(savedUrl.getShortUrl());
+
+		// then
+		assertNotNull(result);
+		assertEquals(originUrl, result.getOriginUrl());
+		assertEquals(shortUrl, result.getShortUrl());
+	}
+
+	@Test
+	@DisplayName("존재하지 않는 단축url로 원본 url 조회 시 실패한다.")
+	public void getOriginUrl_fail_not_exist() {
+		// given:
+		String nonExistentShortUrl = "aabbccddeeggff";
+
+		// when & then
+		Exception exception = assertThrows(NoSuchElementException.class, () -> {
+			shortUrlService.getOriginUrl(nonExistentShortUrl);
+		});
+
+		assertEquals("요청정보가 존재하지 않습니다.", exception.getMessage());
 	}
 }
