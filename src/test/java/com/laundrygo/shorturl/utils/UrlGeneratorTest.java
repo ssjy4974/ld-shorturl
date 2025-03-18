1054b2c4ac44bebaf0294e507537569bbc4d19ed
junyong.seon <ssjy4974@naver.com>
2025-03-19 02:52:28 +0900

    [test] UrlValidatorTest
    - 필수값 여부, 단축url 생성길이, 패턴검중

--- src/test/java/com/laundrygo/shorturl/utils/UrlGeneratorTest.java
+++ src/test/java/com/laundrygo/shorturl/utils/UrlGeneratorTest.java
@@ -2,18 +2,16 @@ package com.laundrygo.shorturl.utils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.security.NoSuchAlgorithmException;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
-@SpringBootTest
 class UrlGeneratorTest {
 
 	@Test
 	@DisplayName("원본 url을 입력받아 8자의 단축 url을 생성하여 반환한다.")
-	void createShortenUrl() throws NoSuchAlgorithmException {
+	void createShortenUrl() {
 		// given
 		String originUrl = "http://www.naver.com";
 		// when
@@ -22,22 +20,10 @@ class UrlGeneratorTest {
 		assertEquals(8, shorten.length());
 	}
 
-	@Test
+	@ParameterizedTest
+	@NullAndEmptySource
 	@DisplayName("입력받은 원본 url이 null인경우 예외를 반환한다.")
-	void originUrlIsNull() throws Exception {
-		// given
-		String originUrl = null;
-		// when then
-		assertThrows(IllegalArgumentException.class, () -> {
-			UrlGenerator.shorten(originUrl);
-		});
-	}
-
-	@Test
-	@DisplayName("입력받은 원본 url이 비어있는경우 예외를 반환한다.")
-	void originUrlIsEmpty() {
-		// given
-		String originUrl = "";
+	void originUrlIsNull(String originUrl) {
 		// when then
 		assertThrows(IllegalArgumentException.class, () -> {
 			UrlGenerator.shorten(originUrl);
